6cfe29824c750d0eeee54b9b115e2779276c60fe
Yarielthe4th <yariel.beato25@compscihigh.org>
2025-04-08 14:01:30 +0000

    done

--- public/script.js
+++ public/script.js
@@ -1,7 +1,43 @@
-// Create a function that tests the post route handler [2 pts]
+// Create a function that tests the post route handler [2 pts
 
+async function newPotion() {
+    try {
+        const response = await fetch(base + "/create-potion", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+              
+         })
+
+        })}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+async function newPotion() {
+    const response = await fetch("/create-potion")
+    const d = response.json()
+}
 
 
 // Call the function [1 pt]
+newPotion()
 // Show us the successful POST fetch [1 pt]
 
